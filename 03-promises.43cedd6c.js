5ff2d453c05213a79c0d7d23ec4b6da7d94bb04a
Lart28 <105484474+Lart28@users.noreply.github.com>
2023-03-11 11:48:29 +0000

    Deploying to gh-pages from @ Lart28/js-09@38c569878b2716a6d6481b854abd9a457e1c1cec 🚀

---
+++ 03-promises.43cedd6c.js
@@ -0,0 +1,2 @@
+!function(){var o,n;console.log("result"),console.log("result"),o=2,n=2e3,new Promise((function(e,c){var l=Math.random()>.3;setTimeout((function(){l?console.log("✅ Fulfilled promise ".concat(o," in ").concat(n,"ms")):console.log("❌ Rejected promise ".concat(o," in ").concat(n,"ms"))}),n)})).then((function(o){console.log(o)}))}();
+//# sourceMappingURL=03-promises.43cedd6c.js.map
