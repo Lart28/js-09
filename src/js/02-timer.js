38c569878b2716a6d6481b854abd9a457e1c1cec
Lart28 <saref28@gmail.com>
2023-03-11 13:47:43 +0200

    2, 3 задание

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,14 +1,7 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
 
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
-const flatpickr = require("flatpickr");
 const selectDateEl = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('[data-start]');
 const daysTimerEl = document.querySelector('[data-days]');
@@ -16,15 +9,23 @@ const hoursTimerEl = document.querySelector('[data-hours]');
 const minsTimerEl = document.querySelector('[data-minutes]');
 const secsTimerEl = document.querySelector('[data-seconds]');
 let timerId = null;
-// const selectedDate = new Date(selectDateEl.value);
-import "flatpickr/dist/flatpickr.min.css";
-flatpickr('#datetime-picker', options)
-// selectDateEl.addEventListener('change', isCorrect(selectedDate));
-startBtn.addEventListener('click', () => {
-  clearInterval(timerId);
-  timer.start(selectDateEl.value);
-})
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
 
+  onClose(selectedDates) {
+    const currentTime = Date.now();
+
+    if (selectedDates[0].getTime() <= currentTime) {
+      startBtn.setAttribute('disabled', '');
+      Notiflix.Notify.failure("Please choose a date in the future");
+    } else {
+      startBtn.removeAttribute('disabled', '');
+    }
+  },
+};
 const timer = {
   start(selectedTime) {
     const startTime = new Date(selectedTime);
@@ -32,15 +33,26 @@ const timer = {
     timerId = setInterval(() => {
       const currentTime = Date.now();
       const deltaTime = startTime - currentTime;
-      const {days, hours, mins, secs} = getTimeComponents(deltaTime);
+      const { days, hours, mins, secs } = getTimeComponents(deltaTime);
+
+      if (deltaTime <= 1000) {
+        clearInterval(timerId);
+      }
       
       daysTimerEl.textContent = `${days}`;
       hoursTimerEl.textContent = `${hours}`;
       minsTimerEl.textContent = `${mins}`;
       secsTimerEl.textContent = `${secs}`;
-    },1000);
+    }, 1000);
   }
 }
+
+flatpickr('#datetime-picker', options)
+
+startBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  timer.start(selectDateEl.value);
+})
 
 function pad(value) {
   return String(value).padStart(2, '0');
@@ -54,14 +66,3 @@ function getTimeComponents(time) {
 
   return { days, hours, mins, secs };
 }
-
-// function isCorrect(date) {
-
-//   console.log('(: ', selectDateEl.value);
-
-//   if (date.getTime() <= Date.now()) {
-//     startBtn.setAttribute('disabled', '')
-//   } else {
-//     startBtn.removeAttribute('disabled', '')
-//   }
-// }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,21 @@
+console.log('result');
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        // Reject
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay)
+  })
+  promise.then(result => {
+    console.log(result);
+  })
 }
+console.log('result');
+createPromise(2, 2000)
