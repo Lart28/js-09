f0ebb2ae092970a6634e596582196d53fada9a40
Lart28 <105484474+Lart28@users.noreply.github.com>
2023-03-12 19:03:23 +0000

    Deploying to gh-pages from @ Lart28/js-09@bed3ca6bce20c06f9cec420c41952c13d72b61e3 🚀

--- 03-promises.5b8ce09c.js
+++
@@ -1,2 +0,0 @@
-var o,e;console.log("result"),console.log("result"),o=2,e=2e3,new Promise(((l,s)=>{const n=Math.random()>.3;setTimeout((()=>{n?console.log(`✅ Fulfilled promise ${o} in ${e}ms`):console.log(`❌ Rejected promise ${o} in ${e}ms`)}),e)})).then((o=>{console.log(o)}));
-//# sourceMappingURL=03-promises.5b8ce09c.js.map
